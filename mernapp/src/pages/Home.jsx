17d4bd25bb4393ac08bd00f7b309dd6832b861e4
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-18 13:48:36 +0300

    create a workout component

--- mernapp/src/pages/Home.jsx
+++ mernapp/src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import Axios from "axios"
+import WorkoutDetails from '../components/workoutDetails';
 
-export default Home
+
+ const Home = () => {
+  const [workouts, setWorkouts] = useState([]);
+   useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await Axios.get('http://localhost:4000/api/workouts/');
+      
+       
+      setWorkouts(response.data);
+      
+    };
+    fetchWorkouts();
+  }, []);
+
+  
+   return (
+    <div className='home'>
+      <div className='workuts'>
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails  key={workout._id} workout={workout} />
+          ))}
+      </div>
+    </div>
+  );
+};
+ export default Home;

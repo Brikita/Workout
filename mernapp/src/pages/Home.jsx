556564aa0b4772964fb9f2b67b6714bf48a69702
Brian <134489011+Brikita@users.noreply.github.com>
2023-07-18 15:30:11 +0300

    h

--- mernapp/src/pages/Home.jsx
+++ mernapp/src/pages/Home.jsx
@@ -2,31 +2,33 @@
 import React, { useEffect, useState } from 'react';
 import Axios from "axios"
 import WorkoutDetails from '../components/workoutDetails';
+import WorkoutsForm from '../components/WorkoutsForm';
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
 
-
- const Home = () => {
-  const [workouts, setWorkouts] = useState([]);
-   useEffect(() => {
+const Home = () => {
+  const { workouts, dispatch } = useWorkoutContext()
+  useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await Axios.get('http://localhost:4000/api/workouts/');
-      
-       
-      setWorkouts(response.data);
-      
+
+
+      dispatch({ type: 'SET_WORKOUTS', payload: response.data });
+
     };
     fetchWorkouts();
   }, []);
 
-  
-   return (
+
+  return (
     <div className='home'>
-      <div className='workuts'>
+      <div className='workouts'>
         {workouts &&
           workouts.map((workout) => (
-            <WorkoutDetails  key={workout._id} workout={workout} />
+            <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
+      <WorkoutsForm />
     </div>
   );
 };
- export default Home;
+export default Home;
